cd2a5a8fe8ee4e97e43a023addb8f1c78d67088e
hazemphoenix <hazem.mando408@gmail.com>
2025-04-29 13:09:47 +0300

    first commit

--- consumingScript.js
+++
@@ -1,9 +0,0 @@
-console.log(toPascalCase("the quick brown fox")); // returns "The Quick Brown Fox"
-
-console.log(longestWord("Web Development Tutorial")); // returns "Development"
-
-console.log(sortString("javascript")); // returns "aacijprstv"
-
-console.log(commonElements([1, 2, 3], [2, 3, 4])); // returns [2,3]
-
-console.log(removeDuplicates([1, 1, 2, 2, 3, 3, 4, 5])); // returns [1,2,3,4,5]
