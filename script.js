6e129018a29c508cb6bc52465a00625c72b50b94
hazemphoenix <hazem.mando408@gmail.com>
2025-05-04 11:28:55 +0300

    refactor the Car class to be able to render and move multiple cars at a time

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ class Car extends Engine {
   #left;
   #imgSrc;
   #direction = "right";
+  #node;
   #width;
   constructor(t, l, i, w = 200) {
     super();
@@ -23,6 +24,8 @@ class Car extends Engine {
     this.#left = l;
     this.#imgSrc = i;
     this.#width = w;
+    this.#node = document.createElement("img");
+    document.body.appendChild(this.#node);
     this.renderCar();
   }
 
@@ -77,15 +80,15 @@ class Car extends Engine {
   }
 
   renderCar() {
-    document.body.innerHTML = "";
-    const img = document.createElement("img");
-    img.src = this.#imgSrc;
-    img.setAttribute("width", `${this.#width}`);
-    img.style.position = "absolute";
-    img.style.top = `${this.#top}px`;
-    img.style.left = `${this.#left}px`;
-    if (this.#direction == "right") img.style.transform = "scaleX(-1)";
-    document.body.appendChild(img);
+    document.body.removeChild(this.#node);
+    this.#node.src = this.#imgSrc;
+    this.#node.setAttribute("width", `${this.#width}`);
+    this.#node.style.position = "absolute";
+    this.#node.style.top = `${this.#top}px`;
+    this.#node.style.left = `${this.#left}px`;
+    if (this.#direction == "right") this.#node.style.transform = "scaleX(-1)";
+    else this.#node.style.transform = "scaleX(1)";
+    document.body.appendChild(this.#node);
   }
 
   changeDirection() {
@@ -94,4 +97,10 @@ class Car extends Engine {
   }
 }
 
-let car = new Car(100, 0, "./images/car-free-svg-cut-file-1.png", 300);
+let car1 = new Car(50, 0, "./images/car-free-svg-cut-file-1.png", 300);
+let car2 = new Car(300, 0, "./images/car-free-svg-cut-file-1.png", 250);
+let car3 = new Car(600, 0, "./images/car-free-svg-cut-file-1.png", 200);
+
+car1.moveCar("right");
+car2.moveCar("right");
+car3.moveCar("right");
