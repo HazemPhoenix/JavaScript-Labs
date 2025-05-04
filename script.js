d47236edca8a232e11740a8eaadb4085d4ef65b5
hazemphoenix <hazem.mando408@gmail.com>
2025-05-04 11:40:48 +0300

    small change and add more car pics

--- script.js
+++ script.js
@@ -88,7 +88,7 @@ class Car extends Engine {
     this.#node.style.position = "absolute";
     this.#node.style.top = `${this.#top}px`;
     this.#node.style.left = `${this.#left}px`;
-    if (this.#direction == "right") this.#node.style.transform = "scaleX(-1)";
+    if (this.#direction == "left") this.#node.style.transform = "scaleX(-1)";
     else this.#node.style.transform = "scaleX(1)";
     document.body.appendChild(this.#node);
   }
@@ -99,9 +99,9 @@ class Car extends Engine {
   }
 }
 
-let car1 = new Car(50, 0, "./images/car-free-svg-cut-file-1.png", 300);
-let car2 = new Car(300, 0, "./images/car-free-svg-cut-file-1.png", 250, 5);
-let car3 = new Car(600, 0, "./images/car-free-svg-cut-file-1.png", 250, 1);
+let car1 = new Car(50, 0, "./images/car1.png", 300);
+let car2 = new Car(420, 0, "./images/car2.svg", 250, 1);
+let car3 = new Car(600, 0, "./images/car3.svg", 250, 5);
 
 car1.moveCar("right");
 car2.moveCar("right");
